f0615f4527083c84e2787902cac0b66f82495470
YukaLangbuana <langbuana.yuka@gmail.com>
2022-12-23 08:15:09 +0700

    rapid data emit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	server.GET("api/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	go func() {
+		for {
+			// Replace this block with data write to central-db
+			fmt.Println("RAPIDSERVICE EMITTING DATA!!")
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	server.Run()
+}
